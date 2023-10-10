5a0042c6b08057c5ca5bff5773421c522ba19b8a
labeebarbisoft <muhammad.labeeb@arbisoft.com>
2023-10-10 12:26:57 +0500

    added content models

---
+++ lms/apps/content/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Profile, Course, Section, SubSection, Unit, Component
+
+
+admin.site.register(Profile)
+admin.site.register(Course)
+admin.site.register(Section)
+admin.site.register(SubSection)
+admin.site.register(Unit)
+admin.site.register(Component)
---
+++ lms/apps/content/models.py
@@ -0,0 +1,68 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    ROLE_TYPES = [
+        ("student", "Student"),
+        ("author", "Author"),
+    ]
+    role = models.CharField(max_length=20, choices=ROLE_TYPES, blank=False)
+
+    def __str__(self):
+        return f"{self.user.username} {self.role}"
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance, role="author")
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} by {self.author}"
+
+
+class Section(models.Model):
+    name = models.CharField(max_length=100)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class SubSection(models.Model):
+    name = models.CharField(max_length=100)
+    Section = models.ForeignKey(Section, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Unit(models.Model):
+    name = models.CharField(max_length=100)
+    sub_section = models.ForeignKey(SubSection, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Component(models.Model):
+    name = models.CharField(max_length=100)
+    content = models.CharField(max_length=10000)
+    unit = models.ForeignKey(Unit, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name}"
