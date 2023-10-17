05a3c02dfed58ec272d7791121d325e7f5b076ef
labeebarbisoft <muhammad.labeeb@arbisoft.com>
2023-10-17 15:48:34 +0500

    updated permissions

--- lms/apps/learning/permissions.py
+++ lms/apps/learning/permissions.py
@@ -3,10 +3,10 @@ from rest_framework import permissions
 
 class IsUserVerified(permissions.BasePermission):
     def has_permission(self, request, view):
-        return request.user.profile.is_verified
+        return request.profile.is_verified
 
 
 class IsUserStudent(permissions.BasePermission):
     def has_permission(self, request, view):
-        return True
-        return request.user.profile.role == "student"
+        # return True
+        return request.profile.role == "student"
